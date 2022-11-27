bd6f52c5f43f76bcaa85f2eec77653f2d2bf82ae
Mark Cohen <Mark.Cohen1@marist.edu>
2022-11-26 22:54:44 -0500

    Time domain is working, accuracy is low tho. Needs cleanup

--- src/main/java/frm/TimeDomainFRM.java
+++ src/main/java/frm/TimeDomainFRM.java
@@ -1,19 +1,25 @@
 package frm;
 
-import org.encog.bot.BotUtil;
+import org.encog.Encog;
+import org.encog.ml.MLMethod;
+import org.encog.ml.MLRegression;
+import org.encog.ml.MLResettable;
+import org.encog.ml.data.MLData;
+import org.encog.ml.data.MLDataSet;
 import org.encog.ml.data.temporal.TemporalDataDescription;
 import org.encog.ml.data.temporal.TemporalMLDataSet;
 import org.encog.ml.data.temporal.TemporalPoint;
+import org.encog.ml.factory.MLMethodFactory;
+import org.encog.ml.factory.MLTrainFactory;
+import org.encog.ml.train.MLTrain;
+import org.encog.ml.train.strategy.RequiredImprovementStrategy;
+import org.encog.neural.networks.training.propagation.manhattan.ManhattanPropagation;
 import org.encog.util.arrayutil.NormalizationAction;
 import org.encog.util.arrayutil.NormalizedField;
 import org.encog.util.csv.ReadCSV;
+import org.encog.util.simple.EncogUtility;
 
 import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 public class TimeDomainFRM {
     /**
@@ -49,11 +55,13 @@ public class TimeDomainFRM {
         return dataSet;
     }
 
-    //public static MLRegression trainModel(
-
+    /**
+     * Create a temporal data set to train the network with
+     * @param rawFile the raw input file object
+     * @return temporal data set
+     */
     public static TemporalMLDataSet createTraining(File rawFile) {
         TemporalMLDataSet trainingData = initDataSet();
-        String s = rawFile.toString();
         ReadCSV csv = new ReadCSV(rawFile.toString(), true, ',');
         while (csv.next()) {
             // Date format is yyyy-mm-dd, we'll pare it into yyyymmdd to be used as a sequence integer
@@ -76,15 +84,107 @@ public class TimeDomainFRM {
         return trainingData;
     }
 
-    //public static TemporalMLDataSet predict(File rawFile, MLRegression model)
+    /**
+     * Create and train a model using Encog factory codes to specify the model type.
+     * @param trainingData The temporal training data set
+     * @param methodName
+     * @param methodArchitecture
+     * @param trainerName
+     * @param trainerArgs
+     * @return
+     */
+    public static MLRegression trainModel(
+            MLDataSet trainingData,
+            String methodName,
+            String methodArchitecture,
+            String trainerName,
+            String trainerArgs) {
+
+        // first, create the machine learning method (the model)
+        MLMethodFactory methodFactory = new MLMethodFactory();
+        MLMethod method = methodFactory.create(
+                methodName,
+                methodArchitecture,
+                trainingData.getInputSize(),
+                trainingData.getIdealSize()
+        );
+
+        // second, create the trainer
+        MLTrainFactory trainFactory = new MLTrainFactory();
+        MLTrain train = trainFactory.create(method, trainingData, trainerName, trainerArgs);
+
+        // reset if improve is less than 1% over 5 cycles
+        if( method instanceof MLResettable && !(train instanceof ManhattanPropagation) ) {
+            train.addStrategy(new RequiredImprovementStrategy(500));
+        }
+
+        // third train the model
+        EncogUtility.trainToError(train, 0.002);
+
+        return (MLRegression)train.getMethod();
+    }
+
+    public static TemporalMLDataSet predict(File rawFile, MLRegression model) {
+        TemporalMLDataSet trainingData = initDataSet();
+        ReadCSV csv = new ReadCSV(rawFile.toString(), true, ',');
+        while (csv.next()) {
+            // Date format is yyyy-mm-dd, we'll pare it into yyyymmdd to be used as a sequence integer
+            String dateString = csv.get(0);
+            String modifiedDate = dateString.replace("-", "");
+            int sequenceNumber = Integer.parseInt(modifiedDate);
+
+            double interestRate = csv.getDouble(1);
+
+            // do we have enough data for a prediction yet?
+            if (trainingData.getPoints().size() >= trainingData.getInputWindowSize()) {
+                // Make sure to use index 1, because the temporal data set is always one ahead
+                // of the time slice its encoding.  So for RAW data we are really encoding 0.
+                MLData modelInput = trainingData.generateInputNeuralData(1);
+                MLData modelOutput = model.compute(modelInput);
+                double predictedRate = normRate.deNormalize(modelOutput.getData(0));
+                System.out.println(sequenceNumber + ":Predicted=" + predictedRate + ",Actual=" + interestRate);
+
+                // Remove the earliest training element.  Unlike when we produced training data,
+                // we do not want to build up a large data set.  We just add enough data points to produce
+                // input to the model.
+                trainingData.getPoints().remove(0);
+            }
+
+            // Add the next point to the temporal data set
+            TemporalPoint point = new TemporalPoint(trainingData.getDescriptions().size());
+            point.setSequence(sequenceNumber);
+            point.setData(0, normRate.normalize(interestRate));
+            trainingData.getPoints().add(point);
+        }
+        csv.close();
+
+        // generate the time-boxed data
+        trainingData.generate();
+        return trainingData;
+    }
 
     /**
      * The main method.
      * @param args The arguments.
      */
     public static void main(String[] args) {
-        File rawFile = new File(MYDIR, "data/MORTGAGE30US.csv");
+        // Get the raw mortgage rate training data
+        File rawFile = new File(MYDIR, "MORTGAGE30US.csv");
 
+        // Build the training data set
+        TemporalMLDataSet trainingData = createTraining(rawFile);
+
+        // Build the model and train
+        MLRegression model = trainModel(
+                trainingData,
+                MLMethodFactory.TYPE_FEEDFORWARD,
+                "?:B->SIGMOID->25:B->SIGMOID->?",
+                MLTrainFactory.TYPE_RPROP,
+                "");
+
+        predict(rawFile, model);
+
+        Encog.getInstance().shutdown();
 
     }
 
